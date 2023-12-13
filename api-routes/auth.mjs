7c61ca3dbb723c10b8d0d0c2bdb7417d9426cfdf
tomokinpm <tomoki-goalie@hotmail.com>
2023-12-13 00:24:28 -0800

    add auth function

--- api-routes/auth.mjs
+++ api-routes/auth.mjs
@@ -3,6 +3,7 @@ import { body, validationResult } from "express-validator";
 const router = express.Router();
 import { User } from "../db/user.mjs";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 router.get("/", (req, res) => {
   res.send("Hello auth");
@@ -30,11 +31,41 @@ router.post(
 
     // cripting password
     let hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
+    // console.log(hashedPassword);
 
     //save it to db
     User.push({ email, password: hashedPassword });
+
+    //issue JWT
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    return res.json({ token: token });
   }
 );
 
+//login api
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = User.find((user) => user.email === email);
+  if (!user) {
+    return res.status(400).json([{ message: "User doesnt exist" }]);
+  }
+
+  //crypting and comparing password
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json([{ message: "Incorrect password" }]);
+  }
+
+  const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+  return res.json({ token: token });
+});
+
+//checking if user exists
+router.get("/allUsers", (req, res) => {
+  return res.json(User);
+});
 export default router;
